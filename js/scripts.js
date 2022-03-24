388351491ec6fd2cb5d9efc7e1b2c0309f40fcf9
Sploob <crayiphone@gmail.com>
2022-03-24 13:36:27 -0700

    refactor scalene conditional statement
    Co-Authored-by: Mike Wright <m.wright92@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ function sideCount(side1, side2, side3) {
     $(".output").text("Equilateral");
   }else if (side1 === side2 || side2 === side3 || side1 === side3) {
     $(".output").text("Isosceles");
-  }else if (side1 !== side2 && side1 !== side3 && side2 !== side3) {
+  }else if (side1 !== (side2 && side3) && side2 !== (side1 && side3)) {
     $(".output").text("Scalene");
   }else {
     alert("I'm not sure how we got to this point, but you messed up");
