9f95c19bf35846d8bbf687e0d42b24883cedf5c5
Sploob <crayiphone@gmail.com>
2022-03-24 13:55:16 -0700

    refactor equilateral and remove need for error
    Co-Authored-by: Mike Wright <m.wright92@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -12,17 +12,16 @@ $(document).ready(function() {
   }
   });
 });
-
+// side 1 === ((side2 + side3)/2)
 
 
 function sideCount(side1, side2, side3) {
-  if (side1 === side2 && side1 === side3) {
+  if (side1 === ((side2 + side3)/2)) {
     $(".output").text("Equilateral");
   }else if (side1 === side2 || side2 === side3 || side1 === side3) {
     $(".output").text("Isosceles");
-  }else if (side1 !== (side2 && side3) && side2 !== (side1 && side3)) {
-    $(".output").text("Scalene");
   }else {
-    alert("I'm not sure how we got to this point, but you messed up");
+    $(".output").text("Scalene");
   }
+  
 }
